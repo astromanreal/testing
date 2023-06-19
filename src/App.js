406df72b04af32718eeecd25b79cc8c420b4a804
Manish <astroman6569@gmail.com>
2023-06-19 15:05:26 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Home from './Components/Home';
+
+function App() {
+  return (
+ <>
+<div className='my-container flex-item-center'> 
+<Home/>
+</div>
+ </>
+  );
+}
+
+export default App;
---
+++ src/Components/Home.js
@@ -0,0 +1,28 @@
+export default function Home() {
+  return (
+    <>
+      <h1>Welcome to Shikharji, the Holiest sites for Jains. </h1>
+      <h1>Overview</h1>
+      <p>
+        Shikharji is one of the holiest pilgrimage sites for Jains. It is
+        believed that here is where twenty of the twenty-four Jain Tirthankaras
+        had attained moksha (in other words, freedom from the cycle of rebirth),
+        along with several other monks which makes it one of the most important
+        and holy sites for Jain Tirthas.
+      </p>
+      <h2>Location:</h2>
+      <p>
+        Located on the Parasnath Hill, the highest mountain in the state of
+        Jharkhand, in an inland part of rural east India, of the Giridih
+        district of Jharkhand. Shikharji rises to a height of 1,350 m
+        thus making it the highest mountain in the state.
+      </p>
+      <h3>
+        {" "}
+        It is the most important Jain Tirtha site, for it is the place where
+        twenty of the twenty-four Jain tirthankaras (supreme preachers of
+        Dharma) along with many other monks attained Moksha.
+      </h3>
+    </>
+  );
+}
